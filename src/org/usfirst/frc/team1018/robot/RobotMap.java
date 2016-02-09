4d557b4e25c71d22909c7eb0809d2542a9634cf4
greaperkam <animereaper6@gmail.com>
2016-02-08 19:27:06 -0500

    Put in DriveTrain subsystem and commands

--- src/org/usfirst/frc/team1018/robot/RobotMap.java
+++ src/org/usfirst/frc/team1018/robot/RobotMap.java
@@ -1,4 +1,8 @@
 package org.usfirst.frc.team1018.robot;
+
+import edu.wpi.first.wpilibj.Jaguar;
+import edu.wpi.first.wpilibj.Victor;
+
 /**
  * The RobotMap is a mapping from the ports sensors and actuators are wired into
  * to a variable name. This provides flexibility changing wiring, makes checking
@@ -6,6 +10,13 @@ package org.usfirst.frc.team1018.robot;
  * floating around.
  */
 public class RobotMap {
+	
+    public static Jaguar frontLeft = new Jaguar(1);
+    public static Jaguar frontRight = new Jaguar(3);
+    public static Victor backLeft = new Victor(2);
+    public static Victor backRight = new Victor(4);
+    	
+    
     // For example to map the left and right motors, you could define the
     // following variables to use with your drivetrain subsystem.
     // public static int leftMotor = 1;
